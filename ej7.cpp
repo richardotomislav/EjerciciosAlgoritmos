7b182aeb80ce4bd225ae1c04cee5facf8366faf4
jesusjmuji <al427616@uji.es>
2025-03-10 21:57:16 +0100

    remove ej7.c++ and add .gitignore and ej7.cpp with initial implementation

---
+++ ej7.cpp
@@ -0,0 +1,16 @@
+/*
+
+Tenemos n ≥ 2 datos de tipo real almacenados en un vector y queremos encontrar, entre todos los posibles pares de datos, un par tal que la diferencia entre los dos valores del par sea mínima (por ejemplo, para emparejar a los dos jugadores que tienen la puntuación más similar en un videojuego). ¿Cómo resolverías el problema, y qué coste tendría, sin ordenar el vector? ¿Sería mejor ordenar primero el vector con un algoritmo de coste O(n2) y aprovechar después esa ordenación? ¿Sería mejor ordenarlo con un algoritmo de coste O(n log n) y aprovechar después esa ordenación? ¿Qué harías si no fuese posible modificar el vector y cómo afectaría eso al coste temporal?
+
+    4, 20, 12, 16, 28, 3,
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+}
